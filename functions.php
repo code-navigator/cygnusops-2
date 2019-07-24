e489a23dc15888af22d33405f66cd2f059560309
Mark Dean <markbdean1@gmail.com>
2019-07-24 12:44:33 -0700

    Add custom endpoint for wordpress navigation menu

--- functions.php
+++ functions.php
@@ -40,4 +40,39 @@ function cygnusops_enqueue_scripts() {
   // Enqueue icons
   wp_enqueue_style('icons');
 }
-add_action( 'wp_enqueue_scripts', 'cygnusops_enqueue_scripts');
+add_action( 'wp_enqueue_scripts', 'cygnusops_enqueue_scripts' );
+
+
+
+function cygnusops_register_menu() {
+  register_nav_menu( 'menu', __( 'Header Menu' ) );
+}
+add_action( 'init', 'cygnusops_register_menu' );
+
+
+
+
+
+
+/**
+ * Return menu structure
+ */
+function cygnusops_get_menu() {
+  # Change 'menu' to your own navigation slug.
+  return wp_get_nav_menu_items('main-menu');
+}
+
+/**
+ * Register custom endpoints
+ */
+function cygnusops_register_rest_routes() {
+  // Endpoint for returning menus
+  register_rest_route(
+    'routes',                                 // First URL segment after domain
+    '/menus',                                 // Base URL for route
+    array(
+      'methods' => WP_REST_SERVER::READABLE,  // HTTP method
+      'callback' => 'cygnusops_get_menu',
+  ));
+}
+add_action( 'rest_api_init', 'cygnusops_register_rest_routes' );
---
+++ spa/src/api/api.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+const url 'dev/local';
+
+export default {
+  // GET - request data
+  get (url, request) {
+    const path = new Url(url, request)
+    return axios.get(path.pathWithQuery)
+  },
+  // POST - send data
+  post (url, request) {
+    const path = new Url(url)
+    return axios.post(path.path, request)
+  },
+  // PUT - insert data
+  put (url, request) {
+    const path = new Url(url)
+    return axios.put(path.path, request)
+  },
+  // PATCH - update data
+  patch (url, request) {
+    const path = new Url(url)
+    return axios.patch(path.path, request)
+  },
+  // DELETE - delete data
+  delete (url, request) {
+    const path = new Url(url)
+    return axios.delete(path.path, {data: request})
+  }
+}
